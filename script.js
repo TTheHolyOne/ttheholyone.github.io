d6439a26b26762d3d55dd1bcbd8e9ad7b53faee6
Holy One <69742571+TTheHolyOne@users.noreply.github.com>
2022-03-22 21:24:37 +0000

    yer

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-const setTheme = theme => document.documentElement.className = theme;
+const setTheme = (theme) => {
+  document.documentElement.className = theme;
+  localStorage.setItem('theme', theme);
+}
+const getTheme = () => {
+  const theme = localStorage.getItem('theme');
+  theme && setTheme(theme);
+}
+ 
+getTheme();
