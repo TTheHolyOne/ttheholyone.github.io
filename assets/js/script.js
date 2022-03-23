1c45b17d83e90d962157d591d4bdfff2a0990f74
Holy One <69742571+TTheHolyOne@users.noreply.github.com>
2022-03-23 19:36:53 +0000

    holymods.dev

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Theme Setter
 const setTheme = (theme) => {
   document.documentElement.className = theme;
   localStorage.setItem('theme', theme);
@@ -8,3 +9,62 @@ const getTheme = () => {
 }
  
 getTheme();
+
+
+//Change tabTitle
+const changeTabTitle = () => {
+    const newtitle = document.getElementById("userinput");
+    if (newtitle.value == ""){ //check if the input is blank when they submit
+        window.localStorage.removeItem("title");
+        window.document.title = "HolyMods"
+    } else {
+        window.localStorage.setItem("title", newtitle.value);
+        window.document.title = newtitle.value; //Set window's title to userinput
+    }
+    newtitle.value = ""; //clear input
+};
+
+//Change the tabIcon
+const changeTabIcon = () => {
+    const newfavicon = document.getElementById("userinput");
+    if (validURL(newfavicon.value)){
+        document.head.querySelector("link[rel=icon]").href = newfavicon.value;
+        window.localStorage.setItem("icon", newfavicon.value);
+    } else {
+    }
+    newfavicon.value = ""; //clear input
+};
+
+
+
+//Clears Tab Icon and Title
+const resetTabSettings = () => {
+    let items = ["icon", "title"];
+    items.forEach(item =>
+    window.localStorage.removeItem(item));
+    window.location.reload();
+};
+
+// Classroom hide
+window.addEventListener(
+    'keydown',
+    function(e) {
+        if (e.key == '`') {
+            window.open(
+				"https://classroom.google.com/"
+            )
+        }
+    },
+    false
+)//Declare variables for cloak here
+const local_title = localStorage.getItem("title");
+const local_icon = localStorage.getItem("icon");
+
+//If the window already has title stored in localstorage
+if (window.localStorage.hasOwnProperty("title")) {
+  document.title = local_title;
+}
+//Fetch and set from user's input
+if (window.localStorage.hasOwnProperty("icon")) {
+  document.querySelector("link[rel=icon]").href = local_icon;
+}
