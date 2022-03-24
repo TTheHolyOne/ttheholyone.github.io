28ad4ac1eec8eaf4e246736ed072f2219b6d0371
Holy One <69742571+TTheHolyOne@users.noreply.github.com>
2022-03-24 22:45:20 +0000

    ye

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,3 +47,41 @@ window.addEventListener(
 
 
 
+
+
+// Theme Setter
+const setTheme = (theme) => {
+  document.documentElement.className = theme;
+  localStorage.setItem('theme', theme);
+}
+const getTheme = () => {
+  const theme = localStorage.getItem('theme');
+  theme && setTheme(theme);
+}
+ 
+getTheme();
+
+
+// Classroom hide
+window.addEventListener(
+    'keydown',
+    function(e) {
+        if (e.key == '`') {
+            window.open(
+				"https://classroom.google.com/"
+            )
+        }
+    },
+    false
+)
+
+
+
+
+
+
+
+
+
+
+
